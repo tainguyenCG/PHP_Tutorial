44491c89933142af4a40cc164421a594c8ca6955
ThanhTai2003 <nguyenthanhtai0392k3@gmail.com>
2024-09-24 21:26:37 +0700

    php tutorial

---
+++ index.php
@@ -0,0 +1,8 @@
+
+<?php
+echo "<h1>My first PHP script!</h1>"; //đây là câu lệnh in ra màn hình
+echo "<p>Tôi sẽ học hành nghiêm túc ngôn ngữ lập trình php này </p>";//đây là câu lệnh in ra màn hình
+# cũng là ghi chú nhưng 1 dòng
+// cũng là ghi chú nhưng 1 dòng
+/* cũng là ghi chú nhưng nhiều dòng*/
+?>
