d207225146105e7f46e21acec83bc66c52dbd8eb
ThanhTai2003 <nguyenthanhtai0392k3@gmail.com>
2024-09-24 22:00:13 +0700

    hằng

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 
 <?php
-echo "<h1>Dưới đây là cách đặt tên biến </h1>"; //đây là câu lệnh in ra màn hình
- /**không được đặt tên biến bằng SỐ 
-  * gán biến nếu biến đó là chữ thì để trong "", còn số thì ko cần để. vì nếu số để trong "" thì máy tính sẽ hiểu đó là chữ ko phải số 
-  */
-  $ten = "Tài";
-  $sn = 2003 ;
-  echo $ten . $sn . "Học PHP";
+echo "<h1>Dưới đây là Hằng </h1>"; 
+ define ("hang", "2003");
+ echo hang ;
+
 ?>
