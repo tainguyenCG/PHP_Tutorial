e26f10911dbcd55f76aee8cf8dcc850a93bcf2a9
ThanhTai2003 <nguyenthanhtai0392k3@gmail.com>
2024-09-24 22:20:30 +0700

    sự khác nhau print và echo

--- index.php
+++ index.php
@@ -4,5 +4,8 @@ echo "<h1>Dưới đây là Hằng </h1>";
 $bien = "naylabien";
 define("hang", "2000");
 echo $bien . hang . "chuỗi kí tự nối vào";
+// câu lệnh print giống tương  tự echo nhưng khác về cách thức
+echo "hello"; // còn echo sẽ đếm kí tự, cụ thể hello có 5 chữ sẽ trả về 5
+print "hello"; // cái này dù nhiều chữ nhưng trả về là 1 
 
 ?>
