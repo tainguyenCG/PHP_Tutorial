80fc8f8266290b0cb8a3b7bd619ae234e98dce42
ThanhTai2003 <nguyenthanhtai0392k3@gmail.com>
2024-09-24 21:41:50 +0700

    bien trong php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 
 <?php
-echo "<h1>My first PHP script!</h1>"; //đây là câu lệnh in ra màn hình
-echo "<p>Tôi sẽ học hành nghiêm túc ngôn ngữ lập trình php này </p>";//đây là câu lệnh in ra màn hình
-# cũng là ghi chú nhưng 1 dòng
-// cũng là ghi chú nhưng 1 dòng
-/* cũng là ghi chú nhưng nhiều dòng*/
+echo "<h1>Dưới đây là cách đặt tên biến </h1>"; //đây là câu lệnh in ra màn hình
+ /**không được đặt tên biến bằng SỐ 
+  * gán biến nếu biến đó là chữ thì để trong "", còn số thì ko cần để. vì nếu số để trong "" thì máy tính sẽ hiểu đó là chữ ko phải số 
+  */
+  $ten = "Tài";
+  $sn = 2003 ;
+  echo $ten . $sn . "Học PHP";
 ?>
