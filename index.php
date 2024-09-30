443000c09ea151cfaec5514534bd005b71a841c1
ThanhTai2003 <nguyenthanhtai0392k3@gmail.com>
2024-09-30 21:39:57 +0700

    toán tử phần 2

--- index.php
+++ index.php
@@ -1,78 +1,18 @@
 
 <?php
-// toán tử số học 
-// $a = 15;
-// $b = 25;
-// $c = $a + $b;
-// $d = $b - $a;
-// $e = $a * $b;
-// $g = $a / $b ;
-// echo $c ;
-// echo "<br>";
-// echo $d ;
-// echo "<br>";
-// echo $e ;
-// echo "<br>";
-// echo $g ;
-// toán tử gán 
-// $a = 20;
-// $a += 100;
-// echo $a; 
-// echo "<br>";
-// $b = 320;
-// $b -= 100;
-// echo $b; 
-// echo "<br>";
-// $c = 320;
-// $c *= 100;
-// echo $c; 
-// echo "<br>";
-// $d = 320;
-// $d /= 100;
-// echo $d; 
-// echo "<br>";
-// $e = 320;
-// $e %= 100; // chia lấy dư 
-// echo $e; 
-// echo "<br>";
-// toán tử so sánh 
-// $g = 29000;
-// $u = 2000;
-// if ($g < $u){
-//     echo "biến $g < biến $u";
-// } elseif($g == $u){
-//     echo "biến $g = biến $u";
-// }else{
-//     echo "đẳng thức đang xét bị sai";
+
+// $a = 10;
+// $b = 1;
+// //&& cả 2 điều kiện phải đúng (và)
+// // or và || 1 trong 2 điều kiện đúng (hoặc)
+// // xor 1 cái đúng 1 cái sai thì xuất, còn 2 cái đúng hay 2 cái sai thì ko xuất
+// if($a < 5 && $b < 5){
+//     echo "đúng &&";
+// }elseif( $a < 20 || $b >5){
+//     echo "đúng Or";
 // }
-// so sánh kiểu int với string 
-// $g = 29000;
-// $u = "2000";
-// if ($g === $u){ // === thì nó xét cùng kiểu cùng chất nếu khác thì nó sai, còn == là xét xem nó bằng nhau không
-//     echo "biến $g < biến $u";
-// }else{
-//     echo "đẳng thức đang xét sai";
-// }
-// toán tử khác
-// $a = 20;
-// $b= 300;
-// if ($a != $b){ //     != kí hiệu là khác
-//         echo " $a khác $b";
-//     }else{
-//         echo "đẳng thức đang xét bị sai";
-//     }
-//     echo "<br>";
-// $c = 201;
-// $d= 3006;
-//     if ($c >= $d){ //     >= kí hiệu là lớn hơn bằng
-//             echo " $c >= $d";
-//         }else{
-//             echo "đẳng thức đang xét bị sai";
-//         }
-// echo "<br>";
-// toán tử tăng giảm 
-$a = 10 ;
-echo ++$a; //10 + 1 = 11
-echo "<br>";
-echo --$a; // khi này a = 11 thì --a sẽ thành 11 - 1 = 10 
+$a = "Tôi";
+$b = " là beginer";
+$a .= $b; //chấm bằng là nối chuỗi
+echo $a;
 ?>
