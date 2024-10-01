2f6475a557f95cd92a686c3eb80a466baeb2a653
ThanhTai2003 <nguyenthanhtai0392k3@gmail.com>
2024-10-01 08:02:55 +0700

    Vòng lặp For

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 
 <?php
 
-$mau = "màu đỏ";
-switch ($mau){ // kiểm tra màu đúng biến thì in ra và dừng lại tại break
-    case "màu đỏ";
-        echo "nó là màu đỏ";
-        break;
-    case "màu đen";
-        echo "nó là màu đen";
-        break;
+for ($a = 0; $a <= 10; $a++){
+    echo $a;
 }
 ?>
