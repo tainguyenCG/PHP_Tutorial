47a1b82b9d628e35bb89896c5ac8b280be11a503
ThanhTai2003 <nguyenthanhtai0392k3@gmail.com>
2024-10-01 07:52:27 +0700

    Câu lệnh Switch

--- index.php
+++ index.php
@@ -1,18 +1,13 @@
 
 <?php
 
-// $a = 10;
-// $b = 1;
-// //&& cả 2 điều kiện phải đúng (và)
-// // or và || 1 trong 2 điều kiện đúng (hoặc)
-// // xor 1 cái đúng 1 cái sai thì xuất, còn 2 cái đúng hay 2 cái sai thì ko xuất
-// if($a < 5 && $b < 5){
-//     echo "đúng &&";
-// }elseif( $a < 20 || $b >5){
-//     echo "đúng Or";
-// }
-$a = "Tôi";
-$b = " là beginer";
-$a .= $b; //chấm bằng là nối chuỗi
-echo $a;
+$mau = "màu đỏ";
+switch ($mau){ // kiểm tra màu đúng biến thì in ra và dừng lại tại break
+    case "màu đỏ";
+        echo "nó là màu đỏ";
+        break;
+    case "màu đen";
+        echo "nó là màu đen";
+        break;
+}
 ?>
