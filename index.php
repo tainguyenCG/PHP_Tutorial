001e17f2668fd905e20162f4efa0ebb628695ff0
ThanhTai2003 <nguyenthanhtai0392k3@gmail.com>
2024-10-01 22:57:34 +0700

    YÊU CẦU NGHIÊM NGẶT TRONG PHP

--- index.php
+++ index.php
@@ -1,31 +1,5 @@
+<?php declare(strict_types=1) ; //YÊU CẦU NGHIÊM NGẶT TRONG PHP
 
-<?php
-function ham(){
-    echo "gọi hàm thành công";// cơ bản
-}
-ham();
-echo "<br>";
-//
-function name($ten){
-    echo "gọi hàm thành công $ten";// truyền 1 tham số
-}
-name("Hihi");
-echo "<br>";
-name("kkkk");
-echo "<br>";
-//
-function thongtin($tenn , $namsinh){
-    echo "gọi hàm thành công $tenn và $namsinh <br>";// truyền 2 tham số
-}
-thongtin("very","2000");
-thongtin("batcer","2001");
-echo "<br>";
-//
-function hamm($a ="thương"){
-    echo " hàm thành công $a";// truyền giá trị mặc định gán giá trị vào biến
-}
-hamm();
-echo "<br>";
 // tính tổng cơ bản trả về return
 function phepcong(int $e , int $c){ // int khai báo kiểu dữ liệu số 
     $tong = $e + $c;
