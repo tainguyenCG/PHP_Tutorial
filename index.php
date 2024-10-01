3254a3fc4bb43016beadc208492fddd608be9907
ThanhTai2003 <nguyenthanhtai0392k3@gmail.com>
2024-10-01 21:06:54 +0700

    Hàm isset() cơ bản

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 
 <?php
-
-$bien = array(
-    "Tôi", // 0
-    " tên", //1
-    " Tài", //2
-    " Sn 2003" //3
-);
-//echo count($bien);// đếm coi có bao nhiêu thành phần trong mảng
-//echo $ten[2]; // lưu ý này in ra vị trí thứ 2 trong array, đếm trong lập trình là từ 0 đến n
-for( $i = 0; $i < count($bien); $i++){
-    echo $bien[$i];
+// hàm isset kiểm tra xem biến a có tồn tại ko
+//nó rơi vào 2 trường hợp true và false
+$a = 100;
+if(isset($a)){
+    echo "biến a có tồn tại";
+}else{
+    echo "biến a ko tồn tại";
 }
 ?>
