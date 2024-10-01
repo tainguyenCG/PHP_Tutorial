90b79737c82d4b68a948a73b12c1153cd0ec851c
ThanhTai2003 <nguyenthanhtai0392k3@gmail.com>
2024-10-01 22:47:54 +0700

    Function

--- index.php
+++ index.php
@@ -1,11 +1,35 @@
 
 <?php
-// hàm isset kiểm tra xem biến a có tồn tại ko
-//nó rơi vào 2 trường hợp true và false
-$a = 100;
-if(isset($a)){
-    echo "biến a có tồn tại";
-}else{
-    echo "biến a ko tồn tại";
+function ham(){
+    echo "gọi hàm thành công";// cơ bản
 }
+ham();
+echo "<br>";
+//
+function name($ten){
+    echo "gọi hàm thành công $ten";// truyền 1 tham số
+}
+name("Hihi");
+echo "<br>";
+name("kkkk");
+echo "<br>";
+//
+function thongtin($tenn , $namsinh){
+    echo "gọi hàm thành công $tenn và $namsinh <br>";// truyền 2 tham số
+}
+thongtin("very","2000");
+thongtin("batcer","2001");
+echo "<br>";
+//
+function hamm($a ="thương"){
+    echo " hàm thành công $a";// truyền giá trị mặc định gán giá trị vào biến
+}
+hamm();
+echo "<br>";
+// tính tổng cơ bản trả về return
+function phepcong(int $e , int $c){ // int khai báo kiểu dữ liệu số 
+    $tong = $e + $c;
+    return $tong; //trả về biến tổng
+} echo phepcong(11,20) . "<br>";// khi trả về xong thì echo sẽ in ra, 11 hiểu là biến e và 20 hiểu là biến c
+echo phepcong(151,20);
 ?>
