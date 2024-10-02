1c778e261060d8f9f9e115f88ca7d984c6120bc7
ThanhTai2003 <nguyenthanhtai0392k3@gmail.com>
2024-10-02 13:56:43 +0700

    (Hiển thị, độ dài, thêm, xóa, sửa) array

--- index.php
+++ index.php
@@ -1,26 +1,13 @@
 <?php 
 
 $name = array("thương", " hùng", " xưng"," bá");// array chứa chỉ số 
-echo $name[1] , $name[2], $name[3]."<br>";//0 , 1 , 2 ,3
-//
-$thongtin =array("thương"=>"1999", "hùng"=>"2000");// array dạng Json
-echo $thongtin['thương']."<br>";
-echo $thongtin['hùng']."<br>";
-//
-$mess = array(
-    array("thuong", "1999", "vietnam"),//vị trí 0
-    array("duong", "2000", "vietnam"),//vị trí 1
-    array("hung", "1993", "vietnam"),//vị trí 2
-);      // 0     , 1    ,   2  
-echo $mess[0][0]."<br>";// [0] là vị trí đầu của mảng đa chiều [0]vị trí gọi tới của mảng đó
-echo $mess[0][1]."<br>";
-echo $mess[0][2]."<br>";
-//
-echo $mess[1][0]."<br>";
-echo $mess[1][1]."<br>";
-echo $mess[1][2]."<br>";
-//
-echo $mess[2][0]."<br>";
-echo $mess[2][1]."<br>";
-echo $mess[2][2]."<br>";
+$name[1] = "Thiên hạ";// thay thế phần từ vào mảng (hùng bằng thiên hạ)
+$name[] = "tiền";//thêm phần tử vào mảng
+print_r($name);// hiển thị nguyên mảng
+echo "<br>";
+echo count($name);// độ dài mảng
+echo "<br>";
+unset($name[1]);//xóa phần tử
+print_r($name);
+
 ?>
