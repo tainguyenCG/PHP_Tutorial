22c5cebb38020dabcb3e923021dd3954b13bc572
ThanhTai2003 <nguyenthanhtai0392k3@gmail.com>
2024-10-02 14:12:48 +0700

    Vòng lặp For với Array, 2 cách

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php 
 
-$name = array("thương", " hùng", " xưng"," bá");// array chứa chỉ số 
-$name[1] = "Thiên hạ";// thay thế phần từ vào mảng (hùng bằng thiên hạ)
-$name[] = "tiền";//thêm phần tử vào mảng
-print_r($name);// hiển thị nguyên mảng
-echo "<br>";
-echo count($name);// độ dài mảng
-echo "<br>";
-unset($name[1]);//xóa phần tử
-print_r($name);
+$name = array("thương", " hùng", " xưng"," bá");
+for($a = 0; $a < 4; $a++){ //do vòng lặp trong for này mảng có 4 nên <4 và in ra mảng với biến a cho từng phần tử
+    echo $name[$a];
+    echo "<br>";// xuống dòng
+}
+$bien = array("Tôi"," tên"," Tài");
+foreach($bien as $value){//khi đó nó sẽ in từng phần trong mảng
+    echo "$value <br>"; //đây là xuống dòng mỗi phần
+}
 
 ?>
