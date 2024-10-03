8b097d1f3e3f3c70d9465a303b5a54cf0d03c260
ThanhTai2003 <nguyenthanhtai0392k3@gmail.com>
2024-10-04 00:35:52 +0700

    gộp file dùng require và include

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
-<?php 
-
-$a = 110;
-$b = 200;
-function phepcong(){
-    $GLOBALS['c']  = $GLOBALS['a'] + $GLOBALS['b'];
-}
-phepcong();
-echo $c;
-//biến toàn cầu để truy cập mọi phạm vi $GLOBALS['c'] 
-
+<?php
+require ("xuly.php");// chạy file có trong xuly.php, LƯU Ý: nếu dùng nhiều lần thì nó gộp nhiều lần
+require_once("xuly.php");// gộp 1 lần duy nhất file đó, dù nhiều lệnh như v nó vẫn gộp 1 lần vì trùng tên xuly.php
+echo "<br>";
+include("xuly.php"); // dùng như require
+include_once("xuly.php"); // dùng như require_one
 ?>
