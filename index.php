e6190806a9536bea2514961964faa6739a384d4b
ThanhTai2003 <nguyenthanhtai0392k3@gmail.com>
2024-10-03 11:00:38 +0700

    Sắp xếp Array

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php 
 
-$name = array("thương", " hùng", " xưng"," bá");
-for($a = 0; $a < count($name); $a++){ //dùng count để đếm phần tử và cho biến a < số phần tử đếm được 
-    echo $name[$a];
-    echo "<br>";// xuống dòng
-}
-$bien = array("Tôi"," tên"," Tài");
-foreach($bien as $value){//khi đó nó sẽ in từng phần trong mảng
-    echo "$value <br>"; //đây là xuống dòng mỗi phần
-}
-
+$name = array("D","C","E","F");
+ sort($name);// sắp xếp tăng dần
+echo print_r($name);
+//
+echo "<br>";
+$namee = array("A","B","E","F");
+ rsort($namee);// sắp xếp giảm dần
+echo print_r($namee);
 ?>
