3c7f7da3727bcc98ed4bd2c96c04d550504aab05
ThanhTai2003 <nguyenthanhtai0392k3@gmail.com>
2024-10-03 20:05:50 +0700

    chuyển file từ index sang trang xyly bằng cú pháp này

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php 
 
-$a = "12/10/20000";//   0/1/2
-$mang = explode("/", $a);//hiểu cái / này sẽ nhận biết các thành phần trong string đó
-var_dump($mang); //in ra array đó với kiểu dữ liệu được đổi sang string
+header('location: xuly.php');// chuyển file từ index sang trang xyly bằng cú pháp này 
 
 ?>
